5d07611debcbecbdcc53df2d6fb85b3fc5ae4fe2
oprichniki <alfredobyok@gmail.com>
2026-05-27 20:12:06 -0300

    Update sketch_proximidad_y_servo.ino

--- sketch_proximidad_y_servo.ino
+++ sketch_proximidad_y_servo.ino
@@ -2,22 +2,22 @@
 #include <Servo.h>
 Servo servogy; //nombre del servo
 
-int sigPin =8;
-int servoPin =6;
-int buzzerPin =10;
-int ledPinRojo =11;
-int ledPinVerde =12;
-int duracion;
-float distancia;
+int sigPin =8;  //asigna el pin 8 para el signal  del sensor de proximidad
+int servoPin =6;  //asigna el pin 6 para la señal del servo
+int buzzerPin =10;  //asigna el pin 10 para el buzzer
+int ledPinRojo =11;  //asigna el pin 11 para el led rojo 
+int ledPinVerde =12;  //asigna el pin 12 para el led verde
+int duracion;  //variable que se declara para  despues darle el valor del ultrasonido del sensor
+float distancia;  //variable que se declara para  despues guardar el resultado del calculo de distancia
 int dt = 10;            //cada vez que hay un "delay dt" es para q el prcoesador del arduino descanse
-int serial = 9600;
+int serial = 9600;  //estandar de frecuencia del monitor serial para el  muestreo
 
 void setup() {
 
-  servogy.attach(servoPin);
-  Serial.begin(serial);
+  servogy.attach(servoPin);  //le doy el pin del servo al arduino
+  Serial.begin(serial);  //le doy la frecuencial al monitor serial
   pinMode(buzzerPin, 1);
-  pinMode(ledPinVerde, 1);      //ponemos los pines de los leds y el buzzer en salida
+  pinMode(ledPinVerde, 1);      //ponemos los pines de los leds (rojo y verde) y el buzzer en salida
   pinMode(ledPinRojo, 1);
   servogy.write(0);         //seteamos el servo en 0 grados
 
@@ -37,7 +37,7 @@ void loop() {
   distancia = duracion/58.2;        //se divide en 58.2 para dejarlo en centimetros y se guarda en "distancia"
 
   Serial.print("Distancia: ");
-  Serial.print(distancia);
+  Serial.print(distancia);          // escribimos la distancia en el monitor serial
   Serial.println(" cm");
 
   if (distancia < 50) {             //si está a menos de 50cm se activan el led rojo y el buzzer
