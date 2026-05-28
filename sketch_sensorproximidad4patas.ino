0d768dd65cb640fbf0e865323a91c2b7099f0252
oprichniki <alfredobyok@gmail.com>
2026-05-27 21:26:01 -0300

    Update sketch_sensorproximidad4patas.ino

--- sketch_sensorproximidad4patas.ino
+++ sketch_sensorproximidad4patas.ino
@@ -1,32 +1,24 @@
-
-int sigPin = 2;
+int triggerPin = 2;
+int echoPin = 3;
 int dt = 10;
 int duracion;
 int distancia;
-int nivel;
-int serial = 9600;
 
 void setup() {
-  
-  for (int i=0; i<6 ;i++){ pinMode (pinleds[i], OUTPUT); }
-  Serial.begin(serial);
 
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
 }
 
 void loop() {
 
-  pinMode(sigPin, OUTPUT);
-  delay(dt);
-  digitalWrite(sigPin, 1);
-  delay(dt);
-  digitalWrite(sigPin, 0);
-  pinMode(sigPin, INPUT);
 
-  duracion = pulseIn(sigPin, 1);
+  digitalWrite(triggerPin, 1);
+  delay(dt);
+  digitalWrite(triggerPin, 0);
+
+  duracion = pulseIn(echoPin, 1);
   distancia = duracion/58.2;
-
-  Serial.print("Distancia: ");
-  Serial.print(distancia);
-  Serial.println(" cm");
 
 }
